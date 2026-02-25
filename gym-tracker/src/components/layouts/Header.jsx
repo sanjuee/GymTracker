e807faef26f39f9467f300dc43e2c3bebfe5a84b
unknown <sanjaysathish168@gmail.com>
2026-02-26 01:26:00 +0530

    feat: setup basic routing and folder structure

---
+++ gym-tracker/src/components/layouts/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1 className="text-yellow-500"> Header</h1>
+        </header>
+
+    )
+}
+
+export default Header
---
+++ gym-tracker/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/layouts/Header"
+
+const Home = () => {
+    return (
+        <Header/>
+    )
+}
+
+export default Home
