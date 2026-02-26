03590426f2d05b23414f5a7f5163f7b5e7033df7
unknown <sanjaysathish168@gmail.com>
2026-02-26 23:28:03 +0530

    feat: Add header componet

--- gym-tracker/src/components/layouts/Header.jsx
+++ gym-tracker/src/components/layouts/Header.jsx
@@ -1,7 +1,20 @@
+import { CircleUserRound, Search } from "lucide-react"
+
 const Header = () => {
     return (
-        <header>
-            <h1 className="text-yellow-500"> Header</h1>
+        <header className="bg-[#0a0a0a] px-7 border-b border-[#1B1B1B] h-15 flex items-center justify-between sm:px-20">
+           <h1 className="text-white text-xl  font-outfit tracking-tighter  ">GYM<span className="font-bold">LOG</span></h1>
+           <div className="relative flex items-center gap-1">
+                <Search size={20} className="text-zinc-500"/>
+                <input type="text" 
+                placeholder="Search exercise" 
+                className="bg-transparent w-full border border-[#262626] rounded-full py-1 pl-2.5
+                            outline-none focus-within:border-accent/50 focus-within:ring-1 
+                            focus-within:ring-accent/20 transition-all duration-300 sm:w-80"/>
+           </div>
+           <div>
+                <CircleUserRound size={22}/>
+           </div>
         </header>
 
     )
