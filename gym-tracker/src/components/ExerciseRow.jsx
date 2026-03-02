decfc1dc757b993ccafb14b50c283559ea9b05ce
unknown <sanjaysathish168@gmail.com>
2026-03-02 17:14:59 +0530

    style : Add add exercise card UI

--- gym-tracker/src/components/ExerciseRow.jsx
+++ gym-tracker/src/components/ExerciseRow.jsx
@@ -1,8 +1,9 @@
 import ExerciseCard from "./ExerciseCard"
+import { CirclePlus } from "lucide-react"
 
 function ExerciseRow({ title, exercises }) {
     return (
-        <div className="mb-2">
+        <section className="mb-2">
             <div className="flex items-center justify-between mt-2.5 px-6 mb-1">
                 <h2 className="font-outfit text-zinc-100 uppercase font-semibold text-xl 
                                 tracking-tight">{title}</h2>
@@ -16,9 +17,14 @@ function ExerciseRow({ title, exercises }) {
                     exercise={exercise}
                 />
             ))}
+                <div className="flex flex-col justify-center items-center relative shrink-0 bg-[#131313] w-45 h-46
+                        border-2 border-dashed border-zinc-600 rounded-2xl text-zinc-400">
+                            <CirclePlus size={45}/> 
+                            <span className="font-outfit text-l tracking-tight m-0.5">Add Exercise</span>
+                </div>
             </div>
             <hr className="text-zinc-800 mt-1"/>
-        </div>
+        </section>
 
         
     )
